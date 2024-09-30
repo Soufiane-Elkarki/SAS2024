1b4dae8c4c9a7d27c0cf28fa0a967f8488a8b7ea
soufiane elkarki <soufianeelkarki27@gmail.com>
2024-09-30 18:12:01 +0200

    first commit

--- challenge/boucles/boucle2.c
+++ challenge/boucles/boucle2.c
@@ -2,24 +2,25 @@
 
 int main(){
 
-    int num ;
-    int res = 1;
+    long int num ;
+    long long int res = 1;
 
     printf("Entrez un nomber :");
     scanf("%d", &num);
 
-    if (num <= 0 )
+    if (num < 0 )
     {
         printf(" entrer un nombre entier positif .");
     }
     else{
 
-        for (int  i = num ; i > 0 ; i--) 
+        for (int  i = 1 ; i <= num ; ++i ) {
 
         res *= i ;
+        }
     }
-    
-    printf("%d! = %d\n",num, res);
+   
+    printf("%ld! = %lld\n",num, res);
     
 
     return 0;
